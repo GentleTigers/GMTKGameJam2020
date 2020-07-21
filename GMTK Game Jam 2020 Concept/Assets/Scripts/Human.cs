7f009f3d2ca7dc8a3656614262cd24066ca2655d
Flavia <flavia.brogle@hotmail.com>
2020-07-21 14:55:42 +0200

    Added different walking time for infection stages

--- GMTK Game Jam 2020 Concept/Assets/Scripts/Human.cs
+++ GMTK Game Jam 2020 Concept/Assets/Scripts/Human.cs
@@ -51,8 +51,11 @@ public class Human : MonoBehaviour {
     [SerializeField] private WaypointCollection waypointCollection;
     private float currentWaitTime;
     private bool isWaiting;
+    private static bool STOP_WHEN_INFECTED = false;
 
     private float speed = 2.5f;
+    private float noSymptomsSpeed = 2f;
+    private float infectiousSpeed = 2.5f;
     private Vector3 movement;
     /// <summary>
     /// Used for turning the sprite in the correct direction if it is idle.
@@ -204,7 +207,7 @@ public class Human : MonoBehaviour {
             movement.y = Input.GetAxisRaw("Vertical");
             movement.Normalize();
 
-        } else if (status != HumanStatus.Infected && waypointCollection != null && waypointCollection.LoopEnded == false) {
+        } else if ((status != HumanStatus.Infected || (!STOP_WHEN_INFECTED && stage != InfectionStage.Infectious)) && waypointCollection != null && waypointCollection.LoopEnded == false) {
             if (currentWaitTime > 0) {
                 movement.Set(0, 0, 0);
                 currentWaitTime -= Time.deltaTime;
@@ -259,6 +262,17 @@ public class Human : MonoBehaviour {
             }
             transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(lastMovement.y, lastMovement.x) * Mathf.Rad2Deg - 90);
         } else {
+            float currentSpeed = speed;
+            if (status == HumanStatus.Infected) {
+                switch (stage) {
+                    case InfectionStage.NoSymptoms:
+                        speed = noSymptomsSpeed;
+                        break;
+                    case InfectionStage.Infectious:
+                        speed = infectiousSpeed;
+                        break;
+                }
+            }
             transform.position += movement * speed * Time.fixedDeltaTime;
             transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 90);
             lastMovement = new Vector3(movement.x, movement.y);
